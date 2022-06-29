f645576d341a0744942ba0caa44de7041bd83b02
Oliver Findl <github@findl.sk>
2022-06-29 21:11:06 +0200

    Updated examples

--- examples/vite-vue3/src/components/HelloWorld.vue
+++ examples/vite-vue3/src/components/HelloWorld.vue
@@ -1,19 +1,37 @@
+<script setup>
+import { ref } from "vue";
+
+defineProps({
+	message: String
+});
+
+const count = ref(0);
+</script>
+
 <template>
 	<h1>{{ message }}</h1>
-	<button @click="count++">count is: {{ count }}</button>
-	<p>Edit <code>components/HelloWorld.vue</code> to test hot module replacement.</p>
+	<p>
+		Recommended IDE setup:
+		<a href="https://code.visualstudio.com/" target="_blank">VS Code</a>
+		+
+		<a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
+	</p>
+	<p>
+		<a href="https://vitejs.dev/guide/features.html" target="_blank">
+			Vite Documentation
+		</a>
+		|
+		<a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
+	</p>
+	<button type="button" @click="count++">count is: {{ count }}</button>
+	<p>
+		Edit
+		<code>components/HelloWorld.vue</code> to test hot module replacement.
+	</p>
 </template>
 
-<script>
-"use strict";
-
-export default {
-	name: "HelloWorld",
-	props: {
-		message: String
-	},
-	data: () => ({
-		count: 0
-	})
-};
-</script>
+<style scoped>
+a {
+	color: #42b983;
+}
+</style>
